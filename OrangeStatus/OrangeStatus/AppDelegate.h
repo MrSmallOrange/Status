dfca99565434580851ad05242fdcebc0dded7e11
zhangsan <13413412>
2016-04-21 14:40:57 +0800

    初始化项目

---
+++ OrangeStatus/OrangeStatus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OrangeStatus
+//
+//  Created by ZhangCheng on 16/4/21.
+//  Copyright © 2016年 ZhangCheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
